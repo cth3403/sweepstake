1390ed7f46103e60f1819dd3aa680e2964dbc653
Joe Schulkins <jschulkins@gmail.com>
2014-07-02 15:04:48 +0100

    merged glyphicon button function and introduced functionality to add new team/player

--- js/admin.js
+++ js/admin.js
@@ -1,7 +1,16 @@
-// allow players to register their name and email based on signup codes they were sent 
+// allow players to register their name and email based on signup codes they were sent
 
-var drawn = [], auth_key, auth, set_auth, json, player, table_data = []; 
+var drawn = [], auth_key, auth, set_auth, json, player, table_data = [];
 
+function Team(team_id, name){
+  this.team_id = team_id;
+  this.name = name;
+}
+
+function Player(name,email){
+  this.name = name;
+  this.email = email;
+}
 
 // function to generate registration code
 function makeid()
@@ -28,6 +37,7 @@ function popTable(id, array){
         $('#'+id).append('<tr id="'+id+'_table_data_'+key+'"><td><input type="text" value="'+json[0].players[key].name+'"></td><td><input type="text" value="'+json[0].players[key].email+'"></td><td><button class="glyphicon glyphicon-minus"></button></td><td><button class="glyphicon glyphicon-plus"></button></td></tr>');
       });
     }
+    buttons();
 }
 
 // function to post data to the json file
@@ -40,53 +50,69 @@ function postPHP(data){
     url: 'js/save_file.php',
     data: { 'json': JSON.stringify(data) },
     success: function () {
-      $('#result').text('You are now registered for the World Cup Sweepstake. Good Luck.');
+      $('#result').text('Changes have been saved.');
        },
     failure: function() {alert("Error!");}
   });
 }
 
-// on page load get the json file, fire function calls and set click handlers 
+function buttons(){
+  var array, data_id, index, pos, obj;
+
+  // get details about the button that has been clicked
+  $('.glyphicon:button').click(function(){
+    data_id = $(this).parents('tbody').attr('id');
+
+    if(data_id === 'teams'){
+      array = json[0].teams;
+      obj = new Team("","");
+    }
+    else if(data_id === 'players'){
+      array = json[0].players;
+      obj = new Player("","");
+    }
+
+    var index = $(this).parents('tr').attr('id');
+    index = index.toString().match(/\d+/);
+
+    // add an empty object to the arrays and redraw the tables
+    if($(this).hasClass('glyphicon-plus')){
+      var pos = parseInt(index[0])+1;
+      array.splice(pos,0,obj);
+      if(data_id === 'teams'){
+        popTable(data_id, json[0].teams);
+      }
+      else if(data_id === 'players'){
+        popTable(data_id, json[0].players);
+      }
+
+    }
+
+    // remove the  object from the array and  remove the row
+    else if($(this).hasClass('glyphicon-minus')){
+      array.splice(index[0],1);
+      $(this).parents('tr').remove();
+    }
+
+  });
+
+  // call the makeid function to generate a key
+  $("button:first").click(function() {
+    json[0].signup.auth_key = makeid();
+    $('#code').text(json[0].signup.auth_key);
+  });
+}
+
+
+// on page load get the json file, fire function calls and set click handlers
 $.getJSON('data/data.json', function(data) {
   json = data;
-  if(json[0].signup[0].auth_key != ""){
+  if(json[0].signup[0].auth_key !== ""){
     $('#code').text(json[0].signup.auth_key);
   }
 
   popTable('teams', json[0].teams);
   popTable('players', json[0].players);
 
-  $('.glyphicon-plus:button').click(function() {
-    $(this).parents('tr').after('<tr><td><input type="text" value=""></td><td><input type="text" value=""></td></tr>');
-  });
-
-  $('.glyphicon-minus:button').click(function() {
-    var array;
-    var data_id = $(this).parents('tbody').attr('id');
-
-    if(data_id === 'teams'){
-      array = json[0].teams;
-    }
-    else if(data_id === 'players'){
-      array = json[0].players;
-    }
-
-    var index = $(this).parents('tr').attr('id');
-    index = index.toString().match(/\d+/);
-    array.splice(index[0],1);
-    $(this).parents('tr').remove();
-  });
-
-  $("button:first").click(function() {
-    json[0].signup.auth_key = makeid();
-    $('#code').text(json[0].signup.auth_key);
-  });
 
 });
-
-
-
-
-
-
-
