dbfd79e0b9f75a2c94e155b2ac7d4ab3e41dcd6a
Joe Schulkins <jschulkins@gmail.com>
2014-07-10 16:52:12 +0100

    added 'Auto Draw' function and refactored some code

--- js/random.js
+++ js/random.js
@@ -1,4 +1,8 @@
-var teams = [], names = [], drawn = [], result; 
+var drawn = [], result, jsonData = ['teams','players'], json, xval, xitem, xhtml;
+var types = new Array();
+types['teams'] = new Array();
+types['players'] = new Array();
+types['drawn'] = new Array
 
 function Team(team_id, name){
   this.team_id = team_id;
@@ -13,71 +17,98 @@ function Player(name,email){
 function Drawn(team, team_id){
   this.team = team;
   this.team_id = team_id;
-  this.name;
-  this.email;
+}
+
+// Replace the random button with stop and start
+function autoDraw(){
+
+  $.each(jsonData, function(key, value){
+    if(drawn[i] === undefined ){
+      randomArray(types[value], value);
+      i = i++;
+      setTimeout(function(){autoDraw(i);}, 3000);
+    }
+  });
+
 }
 
 function randomArray(r_array, htmlApp){
-  
+
   // randomize array
   var item = r_array[Math.floor(Math.random()*r_array.length)];
 
   // check to see if the array is empty
   if(r_array.length < 1){
     $('#result').shuffleLetters({"text":"The draw has finished"});
-  } 
+  }
   else{
 
-  // shuffle the letters of the team and player names to add to the suspense and 
-  // put them into the result box
-  $('#'+htmlApp).shuffleLetters({"text": item.name, callback:function(){
-    if(htmlApp === 'names'){
-      $('#drawn').prepend('<p>'+ result.team+'  '+result.name+'</p>');
-    }
-  }});
-  
+  // shuffle the letters of the team and player players to add to the suspense and
+  $('#'+htmlApp).shuffleLetters({"text": item.name});
+
   // add the team info to the Drawn object
   if(htmlApp === 'teams'){
     result = new Drawn(item.name,item.team_id) ;
     drawn.push(result);
   }
-  
+
   // add the player info to the Drawn object
-  if(htmlApp === 'names'){
+  if(htmlApp === 'players'){
     result.name = item.name;
-    result.email = item.email; 
+    result.email = item.email;
   }
 
+  // put them into the result box
+  if(result.team && result.name !== undefined){
+    $('#drawn').prepend('<p>'+ result.team+'  '+result.name+'</p>');
+  }
   // remove the item from the array so it won't be picked up again in the draw
-  this[htmlApp] = jQuery.grep(this[htmlApp], function(value) {
-    return value != item;
+  types[htmlApp] = jQuery.grep(types[htmlApp], function(value) {
+    return value !== item;
   });
 
-  
  }
  }
 
 // get the player and team JSON file and create separate objects
  $.getJSON('data/data.json', function(data) {
+  json = data;
 
-       // create the team object from the datafile
-        $.each(data[0].teams, function(key, value) {
-         var team = new Team(value.team_id, value.name);
-         teams.push(team);
-        });
+  $.each(jsonData, function(key, value){
+    var array = types[value];
+    var obj;
 
-        // create the player object from the datafile
-        $.each(data[0].players, function(key, value) {
-          var player = new Player(value.name, value.email);
-          names.push(player);
-        }); 
- });
+    if(value === 'teams'){
+      $.each(json[0][jsonData[key]], function(key, value) {
+        obj = new Team(value.team_id, value.name);
+        array.push(obj)
+      });
+    }
+    if(value === 'players'){
+      $.each(json[0][jsonData[key]], function(key, value) {
+        obj = new Player(value.name, value.email);
+        array.push(obj)
+      });
+    }
+  });
+});
+
 
 $('#random').click(function(){
-  // un-hide the results box
   if($('#drawn_box').hasClass('hidden')){
     $('#drawn_box').removeClass('hidden');
   }
-  randomArray(teams, 'teams');
-  randomArray(names, 'names');
+  // un-hide the results box
+  $.each(jsonData, function(key, value){
+    randomArray(types[value], value);
+  });
+
+  // do an auto draw by simulating a click event
+  if ($('#autoDraw').is(':checked') && types[0].length > 0){
+    setTimeout(function(){
+      $('#random').trigger('click');
+    },2000);
+
+  }
+
 });
